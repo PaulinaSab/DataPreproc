69ad83c6eb1634af33efeee55a48bd810afb06d4
PaulinaSab <161688782+PaulinaSab@users.noreply.github.com>
2024-02-29 09:25:39 +0200

    Add files via upload

---
+++ uzduotis.R
@@ -0,0 +1,18 @@
+#### 4 užduotis
+
+# Duomenų nuskaitymas
+
+library(sas7bdat)
+setwd(getwd())
+donations = read.sas7bdat("donate.sas7bdat")
+
+# Sutvarkome lentelė pagal tidy standartus įtraukiant naują kintamąjį donate rate
+
+library(tidyr)
+library(tidyverse)
+
+donations = donations %>% pivot_longer(c("Qtr1", "Qtr2", "Qtr3", "Qtr4",), names_to = "quarter", values_to = "donate_rate")
+
+# NaN reikšmės pakeičiamos reikšmių vidurkiu
+
+donations$donate_rate = ifelse(is.nan(donations$donate_rate), as.integer(mean(donations$donate_rate, na.rm = T)), donations$donate_rate)
